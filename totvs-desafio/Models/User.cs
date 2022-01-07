014f2afa36814b538bf82778f8ffe6b931db7264
Giuliano Beltrami <juka2027@live.com>
2022-01-06 22:18:02 -0300

    Adicionar arquivos de projeto.

---
+++ totvs-desafio/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace totvs_desafio.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public DateTime created { get; set; }
+        public DateTime modified { get; set; }
+        public DateTime last_login { get; set; }
+        public Profile profile { get; set; }
+
+    }
+}
